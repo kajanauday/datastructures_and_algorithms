45764d9b1d51e4146a9de38e086a970e7994c73f
Uday Kajana <kajanauday@gmail.com>
2023-09-08 10:05:46 +0530

    from intelliJ

---
+++ dsa/linkedlists/SingleLinkedList.java
@@ -0,0 +1,8 @@
+package dsa.linkedlists;
+public class SingleLinkedList {
+    public static void main(String[] args) {
+        System.out.println("Hi All");
+        SNode snode = new SNode(10);
+        System.out.println(snode.data);
+    }
+}
