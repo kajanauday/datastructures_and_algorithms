2d6a4cc0f2f6c06e5fe003df5aeaa78e3823641b
Uday Kajana <kajanauday@gmail.com>
2023-10-26 11:09:13 +0530

    uniqueMorseRepresentations, cellsInRange, toLowerCase, isAcronym,reverseWords,  checkIfPangram, sortSentence, arrayStringsAreEqual.

--- leetcode/problems/Dispatcher.java
+++ leetcode/problems/Dispatcher.java
@@ -8,7 +8,15 @@ public class Dispatcher {
         Scanner scanner;
         Solution solution   = new Solution();
         Toolkit toolkit = new Toolkit();
-        System.out.println(solution.arrayStringsAreEqual(new String[]{"ab", "c"},new String[]{"a", "bc"}));
+        //System.out.println(solution.uniqueMorseRepresentations(new String[]{"gin","zen","gig","msg"}));
+        //solution.cellsInRange("A1:F1");
+        //System.out.println(solution.toLowerCase("Hi ALL HOw ArE You"));
+        //System.out.println(solution.isAcronym());
+        //System.out.println(solution.reverseWords("Let's take LeetCode contest"));
+        //System.out.println(solution.checkIfPangram("thequickbrownfoxjumpsoverthelazydog"));
+        //System.out.println(solution.sortSentence("Myself2 Me1 I4 and3"));
+        //System.out.println(solution.arrayStringsAreEqual(new String[]{"ab", "cd"},new String[]{"ab", "cd"}));
+        //System.out.println(solution.decodeMessage("eljuxhpwnyrdgtqkviszcfmabo", "zwx hnfx lqantp mnoeius ycgk vcnjrdb"));
         //System.out.println(solution.interpret("(al)G(al)()()G"));
         //System.out.println(solution.defangIPaddr("1.1.1.1"));
         //solution.rotateArray(toolkit.sequentialIntegerArray(16,5),2); //this method will rotate array in clock wise direction by n times//
--- leetcode/problems/Solution.java
+++ leetcode/problems/Solution.java
@@ -1,8 +1,6 @@
 package leetcode.problems;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /*
 this class will hold the methods for solving leet-code problems
@@ -10,8 +8,166 @@ this class will hold the methods for solving leet-code problems
  these methods will be called from Dispatcher class.
 */
 public class Solution {
-    Scanner scanner;
     Toolkit toolkit = new Toolkit();
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] mc = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        StringBuilder stringBuilder = new StringBuilder();
+        int count=0;
+        List<String> uniqueElements = new ArrayList<>();
+        for(int i = 0; i < words.length;i++){
+            for(int j =0; j < words[i].length();j++){
+                stringBuilder.append(mc[(words[i].charAt(j)-'a')]);
+            }
+            if(!uniqueElements.contains(stringBuilder.toString())) {
+                count++;
+                uniqueElements.add(stringBuilder.toString());
+            }
+            stringBuilder.setLength(0);
+        }
+    return uniqueElements.size();
+        //https://leetcode.com/problems/unique-morse-code-words/description/
+        //Runtime
+        //Details
+        //2ms
+        //Beats 90.14%of users with Java
+        //Memory
+        //Details
+        //40.32MB
+        //Beats 64.86%of users with Java
+        //submitted at Oct 26, 2023 11:06
+    }
+    public List<String> cellsInRange(String s) {
+        String firstHalf = s.substring(0,s.indexOf(":"));
+        String secondHalf = s.substring(s.indexOf(":")+1);
+        List<String> cells = new ArrayList<>();
+        char c1=s.charAt(0),c2=s.charAt(3);
+        int n1=s.charAt(1) - '0',n2=s.charAt(4)- '0';
+        for(char ch = c1; ch<=c2;ch++){
+            for(int i = n1; i<=n2;i++){
+                cells.add(ch+""+i);
+            }
+        }
+        return cells;
+        //https://leetcode.com/problems/cells-in-a-range-on-an-excel-sheet/description/
+        //Runtime
+        //Details
+        //5ms
+        //Beats 71.81%of users with Java
+        //Memory
+        //Details
+        //43.78MB
+        //Beats 68.07%of users with Java
+        //kajanauday
+        //submitted at Oct 25, 2023 17:27
+    }
+    public String toLowerCase(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i< s.length(); i++){
+            stringBuilder.append((s.charAt(i)>='A' && s.charAt(i)<='Z')?(char)(s.charAt(i)+32):s.charAt(i));
+        }
+        return stringBuilder.toString();
+        //without toLowerCase() method.
+        //Runtime
+        //Details
+        //1ms
+        //Beats 19.20%of users with Java
+        //Memory
+        //Details
+        //40.72MB
+        //Beats 25.93%of users with Java
+        //https://leetcode.com/problems/to-lower-case/description/
+    }
+    public boolean isAcronym(List<String> words, String s) {
+        if(s.length()==words.size()){
+            for(int i = 0; i< s.length();i++){
+                if(s.charAt(i)!=words.get(i).charAt(0))
+                    return false;
+            }
+            return true;
+        }
+        return false;
+        //Runtime
+        //Details
+        //1ms
+        //Beats 100.00%of users with Java
+        //Memory
+        //Details
+        //43.72MB
+        //Beats 32.46%of users with Java
+        //https://leetcode.com/problems/check-if-a-string-is-an-acronym-of-words/description/
+    }
+    public String reverseWords(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int index = 0;
+        for(int i =0; i< s.length();i++){
+            if(s.charAt(i)==' ' || s.charAt(i)=='\n'){
+                for(int j =i; j>=index; j--){
+                    stringBuilder.append(s.charAt(j));
+                }
+                index = i;
+            }
+        }
+        for(int j = s.length()-1;j>=index;j--){
+            System.out.println(s.charAt(j));
+        }
+        return stringBuilder.toString();
+        //Runtime
+        //Details
+        //7ms
+        //Beats 56.14%of users with Java
+        //Memory
+        //Details
+        //44.19MB
+        //Beats 56.17%of users with Java
+        //https://leetcode.com/problems/reverse-words-in-a-string-iii/description/
+    }
+    public boolean checkIfPangram(String sentence) {
+        for(char i = 'a';i<='z';i++){
+            if(!sentence.contains(i+"")){
+                return false;
+            }
+        }
+        return true;
+        //Runtime
+        //Details
+        //1ms
+        //Beats 85.29%of users with Java
+        //Memory
+        //Details
+        //40.15MB
+        //Beats 94.11%of users with Java
+        //https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
+    }
+    public String sortSentence(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        TreeMap<String, String> sortedMap = new TreeMap<>();
+        int j = 0, last = 0;
+        String key = "", value = "";
+        for(int i = 0; i< s.length(); i++){
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                j = i;
+                while(i< s.length()-1 && s.charAt(i)!=' ' && s.charAt(i)!='\n'){
+                    i++;
+                }
+                sortedMap.put(s.substring(j,i+1),s.substring(last,j));
+                last = i+1;
+            }
+        }
+        String[] keys = sortedMap.keySet().toArray(new String[0]);
+        for(int i =0;i<keys.length;i++){
+            stringBuilder.append(sortedMap.get(keys[i])).append((i == keys.length - 1) ? "" : " ");
+        }
+        return stringBuilder.toString();
+        //Runtime
+        //Details
+        //1ms
+        //Beats 59.46%of users with Java
+        //Memory
+        //Details
+        //39.76MB
+        //Beats 99.43%of users with Java
+        //https://leetcode.com/problems/sorting-the-sentence/description/
+    }
     public int balancedStringSplit(String s) {
         int R=0,L=0;
         for(int i = 0;i<s.length();i++){
@@ -24,21 +180,42 @@ public class Solution {
 
         return 0;
     }
-    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        int i = 0, f = 0, j = 0, s = 0;
-        boolean areSame=true,isUpdated=false;
-        char firstChar = '1', secondChar = '1';
-        while (f < word1.length) {
-            if (i < word1[f].length())
-                firstChar = word1[f].charAt(i);
-            else
-                i++;
-            System.out.println(firstChar);
-            i = i % (word1[f].length());
+    public String decodeMessage(String key, String message) {
+        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder keys = new StringBuilder();
+        StringBuilder values = new StringBuilder();
+        Map<Character, Character> intCharMap = new HashMap<>();
+        intCharMap.put(' ', ' ');
+        char ascci_num = 'a';
+        for (int i = 0; i < key.length(); i++) {
+            if (!intCharMap.containsKey(key.charAt(i))) {
+                if (key.charAt(i) >= 'a' && key.charAt(i) <= 'z')
+                    intCharMap.put(key.charAt(i),ascci_num++);
+            }
         }
-        return true;
+        for (int i = 0; i< message.length();i++)
+            stringBuilder.append(intCharMap.get(message.charAt(i)));
+        return stringBuilder.toString();
+        //Runtime
+        //Details
+        //7ms
+        //Beats 70.79%of users with Java
+        //Memory
+        //Details
+        //42.41MB
+        //Beats 83.08%of users with Java
+        //https://leetcode.com/problems/decode-the-message/description/
+    }
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb = new StringBuilder();
+        for(String s : word1) {
+            sb.append(s);
+        }
+        StringBuilder SB = new StringBuilder();
+        for(String s : word2) {
+            SB.append(s);
+        }
+        return sb.toString().contentEquals(SB);
     }
     public String truncateSentence(String s, int k) {
         int count = 0, i = 0;
@@ -69,11 +246,11 @@ public class Solution {
             index = 1;
         else
             index = 2;
-            for(int i = 0;i<items.size();i++) {
-                if (items.get(i).get(index).equals(ruleValue)) {
-                    count++;
-                }
+        for (List<String> item : items) {
+            if (item.get(index).equals(ruleValue)) {
+                count++;
             }
+        }
             //(CMT) 99.45% , 87.55% , 3ms , https://leetcode.com/problems/count-items-matching-a-rule/
             return count;
     }
